d0971ffa0f7bae0cc04d712018a100ecaa288fd9
matanew1 <matanew1@gmail.com>
2025-12-28 23:18:37 +0200

    Remove post image upload API from upload module

--- src/upload/upload.module.ts
+++ src/upload/upload.module.ts
@@ -4,11 +4,10 @@ import { ConfigModule } from '@nestjs/config';
 import { UploadController } from './upload.controller';
 import { UploadService } from './upload.service';
 import { User } from '../auth/user.entity';
-import { Post } from '../posts/post.entity';
 import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, Post]), ConfigModule, AuthModule],
+  imports: [TypeOrmModule.forFeature([User]), ConfigModule, AuthModule],
   controllers: [UploadController],
   providers: [UploadService],
   exports: [UploadService],
